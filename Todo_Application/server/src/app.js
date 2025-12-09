889a4fb07b473e63a5de1dc2b5185b10fc324ebd
kashif-marofi <marofikashif@gmail.com>
2025-12-09 12:13:54 +0500

    Todo Added

--- Todo_Application/server/src/app.js
+++ Todo_Application/server/src/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
+
 app.use(express.json());
+app.use(cors());
 
 const { connectDB } = require("./config/database");
 const { Todo } = require("./model/todoSchema");
